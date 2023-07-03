b2ab849a18d37942ab5ca572417a9db2ac586388
dmytro <pavlusenko.de@gmail.com>
2023-07-03 12:04:10 +0200

    update for local settings v2

--- src/test/java/Test250.java
+++ src/test/java/Test250.java
@@ -1,11 +1,10 @@
+import com.beust.jcommander.Parameter;
 import com.google.common.base.Verify;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
@@ -15,17 +14,19 @@ import java.util.Random;
 
 public class Test250 {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "/Users/dmytro/chromedriver");
     }
 
     WebDriver driver = new ChromeDriver();
-    @AfterTest
-    public void tearDown() {
-        driver.quit();
-    }
-   @Test
+  @AfterTest
+   public void tearDown() {driver.quit();
+  }
+  @Parameter (names = "pathtodriver")
+  public void dr(String pathtodriver){
+      System.setProperty("webdriver.chrome.driver", pathtodriver);}
+  @Test
     public void Test250() {
         var toplimit = 250;
+        var sitev = 1;
         Movies[] movie = new Movies[toplimit];
         WebElement[] title = new WebElement[toplimit];
         WebElement[] year = new WebElement[toplimit];
@@ -44,56 +45,55 @@ public class Test250 {
         WebElement burger = driver.findElement(By.xpath("//*[@id='imdbHeader-navDrawerOpen']"));
         burger.click();
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        WebElement top = driver.findElement(By.linkText("Top 250 Movies"));
-        top.click();
+      WebElement top = driver.findElement(By.xpath("//*[@id='imdbHeader']/div[2]/aside/div/div[2]/div/div[1]/span/div/div/ul/a[2]"));
+      driver.get(top.getAttribute("href"));
 
 
-        // get class name to check site version
+      //check which site version we have, toggle sitev to 0 if version is old, remains 1 as default when site version new
         WebElement body = driver.findElement(By.cssSelector("body"));
+      if (body.getAttribute("className").contains("fixed")){
+          sitev=0;
+      }
 
-        if (body.getAttribute("className").contains("fixed")) {
-            //get titles data
+        if (sitev==0) {
+            //get data for old version: titles,year, rating
             for (int i = 0; i < toplimit; i++) {
                 title[i] = driver.findElement(By.xpath("//*[@id='main']/div/span/div[1]/div/div[3]/table/tbody/tr[" + Integer.toString(i + 1) + "]/td[2]/a"));
                 movie[i].Title = title[i].getText();
             }
-            //get year data
             for (int i = 0; i < toplimit; i++) {
                 year[i] = driver.findElement(By.xpath("//*[@id='main']/div/span/div[1]/div/div[3]/table/tbody/tr[" + Integer.toString(i + 1) + "]/td[2]/span"));
                 movie[i].Year = year[i].getText().substring(1, 5);
             }
-            //get rating data
             for (int i = 0; i < toplimit; i++) {
                 rating[i] = driver.findElement(By.xpath("//*[@id='main']/div/span/div[1]/div/div[3]/table/tbody/tr[" + Integer.toString(i + 1 ) + "]/td[3]/strong"));
                 movie[i].Rating = rating[i].getText().replace(',','.');
             }
         }else{
-                //get titles data new version
-                for (int i = 0; i < toplimit; i++) {
+            //get data for new version: titles,year, rating
+            for (int i = 0; i < toplimit; i++) {
                     title[i] = driver.findElement(By.xpath("//*[@id='__next']/main/div/div[3]/section/div/div[2]/div/ul/li[" + Integer.toString(i+1) + "]/div[2]/div/div/div[1]/a/h3"));
                     titletext[i] = title[i].getText();
                     movie[i].Title = titletext[i].substring(titletext[i].indexOf(".")+2);
                 }
-                //get year data new version
                 for (int i = 0; i < toplimit; i++) {
                     year[i] = driver.findElement(By.xpath("//*[@id='__next']/main/div/div[3]/section/div/div[2]/div/ul/li[" + Integer.toString(i+1) + "]/div[2]/div/div/div[2]/span[1]"));
                     movie[i].Year = year[i].getText();
                 }
-                //get rating data new version
                 for (int i = 0; i < toplimit; i++) {
                     rating[i] = driver.findElement(By.xpath("//*[@id='__next']/main/div/div[3]/section/div/div[2]/div/ul/li[" + Integer.toString(i + 1) + "]/div[2]/div/div/span/div/span"));
                     movie[i].Rating = rating[i].getText();
                 }
         }
-
+        // define position of random movie
         Random r = new Random();
         int rposition = r.nextInt(toplimit) + 1;
 
        System.out.println("Movie on " + Integer.toString(rposition) + " was checked");
 
-        if (body.getAttribute("className").contains("fixed")) {
+        if (sitev==0) {
             WebElement rmovielink = driver.findElement(By.xpath("//*[@id='main']/div/span/div[1]/div/div[3]/table/tbody/tr[" + Integer.toString(rposition) + "]/td[2]/a"));
             new Actions(driver)
                     .scrollToElement(rmovielink)
@@ -101,32 +101,34 @@ public class Test250 {
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
             rmovielink.click();
         }else{
-
+            //close notification that overlay page
             WebElement clsnt = driver.findElement(By.className("ipc-snackbase__buttons"));
             clsnt.click();
 
-
+            // open random movie page
             WebElement rmovielink = driver.findElement(By.xpath("//*[@id='__next']/main/div/div[3]/section/div/div[2]/div/ul/li[" + Integer.toString(rposition) + "]/div[2]/div/div/div[1]/a"));
             rmovielink.getAttribute("href");
-            System.out.println(rmovielink.getAttribute("href"));
-
             driver.get(rmovielink.getAttribute("href"));
         }
-
+      // get values from random movie page
         WebElement mtitle = driver.findElement(By.xpath("//*[@id='__next']/main/div/section[1]/section/div[3]/section/section/div[2]/div[1]/h1/span"));
         WebElement myear = driver.findElement(By.xpath("//*[@id='__next']/main/div/section[1]/section/div[3]/section/section/div[2]/div[1]/ul/li[1]/a"));
         WebElement mrating = driver.findElement(By.xpath("//*[@id='__next']/main/div/section[1]/section/div[3]/section/section/div[2]/div[2]/div/div[1]/a/span/div/div[2]/div[1]/span[1]"));
+        WebElement mposition = driver.findElement(By.xpath("//*[@id='__next']/main/div/section[1]/div/section/div/div[1]/section[1]/div/div/a"));
 
         String mtitlev = mtitle.getText();
         String myearv = myear.getText();
         String mratingv = mrating.getText();
+        String mpositionv = mposition.getText().substring(17);
+        String rmposlst = String.valueOf(movie[rposition-1].Position);
 
 
+      // compare values on random movie page
+        Verify.verify(mtitlev.equals(movie[rposition-1].Title), "Title isn't matched Value on list:"+movie[rposition-1].Title.toString()+" Value on page:"+mtitlev.toString());
+        Verify.verify(myearv.equals(movie[rposition-1].Year), "Year isn't matched Value on list:"+movie[rposition-1].Year.toString()+" Value on page:"+myearv.toString());
+        Verify.verify(mratingv.equals(movie[rposition-1].Rating), "Rating isn't matched. Value on list:"+movie[rposition-1].Rating.toString()+" Value on page:"+mratingv.toString());
+        Verify.verify(mpositionv.equals(rmposlst), "Position isn't matched. Value on list:"+ rmposlst +" Value on page:"+mpositionv);
 
-
-        Verify.verify(mtitlev.equals(movie[rposition-1].Title), "Title isn't matched Value on list:"+movie[rposition-1].Title.toString()+" Value on page:"+mtitle.getText().toString());
-        Verify.verify(myearv.equals(movie[rposition-1].Year), "Year isn't matched Value on list:"+movie[rposition-1].Year.toString()+" Value on page:"+myear.getText().toString());
-        Verify.verify(mratingv.equals(movie[rposition-1].Rating), "Rating isn't matched. Value on list:"+movie[rposition-1].Rating.toString()+" Value on page:"+mrating.getText().toString());
 
         driver.quit();
     }
--- src/test/java/Test5.java
+++ src/test/java/Test5.java
@@ -1,3 +1,4 @@
+import com.beust.jcommander.Parameter;
 import com.google.common.base.Verify;
 import junit.runner.Version;
 import org.openqa.selenium.By;
@@ -15,13 +16,11 @@ import java.util.Locale;
 
 public class Test5 {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "/Users/dmytro/chromedriver");
     }
-    WebDriver driver = new ChromeDriver();
-    @AfterTest
-    public void tearDown() {
-        driver.quit();
-    }
+
+    @Parameter(names = "pathtodriver")
+    public void dr(String pathtodriver){
+        System.setProperty("webdriver.chrome.driver", pathtodriver);}
    @Test
     public void Test5() {
 
@@ -33,6 +32,7 @@ public class Test5 {
         WebElement[] year = new WebElement[toplimit];
         WebElement[] rating = new WebElement[toplimit];
 
+       WebDriver driver = new ChromeDriver();
 
         for (int i = 0; i < toplimit; i++) {
             movie[i] = new Movies();
@@ -43,10 +43,12 @@ public class Test5 {
 
         WebElement burger = driver.findElement(By.xpath("//*[@id='imdbHeader-navDrawerOpen']"));
         burger.click();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        WebElement top = driver.findElement(By.linkText("Top 250 Movies"));
-        top.click();
+        WebElement top = driver.findElement(By.xpath("//*[@id='imdbHeader']/div[2]/aside/div/div[2]/div/div[1]/span/div/div/ul/a[2]"));
+        driver.get(top.getAttribute("href"));
+
+
 
         //check which site version we have, toggle sitev to 0 if version is old, remains 1 as default when site version new
         WebElement body = driver.findElement(By.cssSelector("body"));
@@ -70,7 +72,7 @@ public class Test5 {
                 movie[i].Rating = rating[i].getText().replace(',','.');
             }
         }else{
-            //close notification that overlay our main page
+            //close notification that overlay page
             WebElement clsnt = driver.findElement(By.className("ipc-snackbase__buttons"));
             clsnt.click();
 
@@ -90,16 +92,15 @@ public class Test5 {
         }
 
 
-        String usercurrlang = Locale.getDefault().toString();
-
+        String usercurrlang = Locale.getDefault().getDisplayLanguage().toString();
+System.out.println(usercurrlang);
 
         switch (usercurrlang) {
-            case "uk_UA": gflocname = "Хрещений батько";
+            case "українська": gflocname = "Хрещений батько";
                 break;
-            case "en_US":
-            case "en_UK": gflocname = "The Godfather";
+            case "English": gflocname = "The Godfather";
                 break;
-            case "ru_RU": gflocname = "Крестный отец";
+            case "русский": gflocname = "Крестный отец";
                 break;
             default: System.out.println("Please set your system language to English, Ukrainian, Ru and rerun test");
                 break;
@@ -113,8 +114,16 @@ public class Test5 {
             // check if movie is present in Top5
             Verify.verify (movieslist.contains(gflocname),"It couldn't happen.'The Godfather' isn't in Top 5");
 
-            WebElement gflink = driver.findElement(By.linkText(gflocname));
-            gflink.click();
+       int position = movieslist.indexOf(gflocname);
+
+            if (sitev==0){
+                WebElement gflink = driver.findElement(By.xpath("//*[@id='main']/div/span/div[1]/div/div[3]/table/tbody/tr[" + Integer.toString(position+1) + "]/td[2]/a"));
+                driver.get (gflink.getAttribute("href"));
+            }else {
+                WebElement gflink = driver.findElement(By.xpath("//*[@id='__next']/main/div/div[3]/section/div/div[2]/div/ul/li[" + Integer.toString(position+1) +"]/div[2]/div/div/div[1]/a"));
+                driver.get (gflink.getAttribute("href"));
+            }
+
 
             WebElement mtitle = driver.findElement(By.xpath("//*[@id='__next']/main/div/section[1]/section/div[3]/section/section/div[2]/div[1]/h1/span"));
             WebElement myear = driver.findElement(By.xpath("//*[@id='__next']/main/div/section[1]/section/div[3]/section/section/div[2]/div[1]/ul/li[1]/a"));
@@ -124,7 +133,7 @@ public class Test5 {
             String myearv = myear.getText();
             String mratingv = mrating.getText();
 
-            int position = movieslist.indexOf(gflocname);
+
 
             Verify.verify(mtitlev.equals(movie[position].Title), "Title isn't matched Value on list:"+movie[position].Title.toString()+" Value on page:"+mtitlev.toString());
             Verify.verify(myearv.equals(movie[position].Year), "Year isn't matched Value on list:"+movie[position].Year.toString()+" Value on page:"+myearv.toString());
